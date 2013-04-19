7ecafcedd1e438c8658db32fe1e1857b211bf145
michael <micmath@udel.edu>
2013-04-19 10:53:01 -0400

    Began laying out file names.

---
+++ src/Parser/Tython_Lexer.py
@@ -0,0 +1,18 @@
+'''
+Created on Apr 19, 2013
+
+@author: michael
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        pass
+        
